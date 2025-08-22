0df44eecf67f56108affe7bfff1e385bfa73dfa2
abdelkadous <development.abdelkadous@gmail.com>
2025-08-22 16:21:57 +0100

    add ✨: defaults like default log file, working dir...

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+pub const DEFAULT_CONFIG_FILE_NAME: &str = ".config";
+pub const DEFAULT_CONFIG_FILE_EXTENSION: &str = "kdl";
+pub const DEFAULT_LOG_DIR: &str = "/var/log/pkg";
+pub const DEFAULT_WORKING_DIR: &str = "/var/tmp/pkg";
+
 pub mod config;
 
 pub mod input;
